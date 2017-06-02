983e92518d5818bb48a4d3dde8482342e39ce0fb
JerryAhn <jb7959@naver.com>
2017-06-02 01:02:09 +0000

    문자열 검색 기능 추가 및 함수 분리, 소문자 변환 추가

--- prime.js
+++ prime.js
@@ -12,7 +12,8 @@ var upload = multer({ dest: './client/tmp/' }); //임시 디렉토리
 //file 읽기용
 var fs = require('fs');
 //MapReduce 용 패키지 
-var mapReduce = require('mapred')();
+var mapReduce = require('mapred')(1);
+
 
 
 //Mapping path of Static file. 사용법 : app.use('클라이언트 URI', express.static((__dirname+"실제 서버 경로"));
@@ -69,30 +70,47 @@ app.get('/2', function (req, res) {
 //파일 읽고, 처리 
 app.post('/upload',upload.single('upload') ,function (req, res) {
  //console.log(req.file.path); //파일경로 읽기
- var readFile= fs.readFileSync(req.file.path,'utf8'); //Reading File at ./client/tmp 동기방식으로 읽고 buffer로 제공 (옵션으로 두번째 인수로 'utf8' 삽입시 String 반환 )
- var readFiles = preProcessing(readFile);
+ var readFile = fs.readFileSync(req.file.path,'utf8'); //Reading File at ./client/tmp 동기방식으로 읽고 buffer로 제공 (옵션으로 두번째 인수로 'utf8' 삽입시 String 반환 )
+ var readFiles = doFinder(readFile,"was",preProcessing,serchingText);
  template(readFiles,res,2); //템플릿엔진을 사용하여 변환
  fs.unlink(req.file.path, function (err) { if (err) throw err; console.log('successfully deleted'+req.file.path); }); //Removing File at ./client/tmp
 });
 
 
+function doFinder(file,serchingText,preProcessingFunc,serchingFunc) {
+   var readFiles = preProcessingFunc(file);
+       readFiles = serchingFunc(readFiles,serchingText);
+       return readFiles;
+}
+
+
 //데이터 전처리 함수
 //정규식 참조 https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/%EC%A0%95%EA%B7%9C%EC%8B%9D#special-or
 function preProcessing(data){
-// 공백 삭제 
- var readFile=data.trim();
+// 맨앞뒤 공백 삭제 및 소문자
+ var readFile=data.trim().toLowerCase();
  //정규식으로 문자열 분리/[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9 | 't)]/gi;   // 특수문자 제거('t 등 제외)'  예) ^는 시작문자 , |는 OR,  "" 는 모든 특수문자,gi는대/소문자 무시하고 완전 일치, \s 는 모든 공백 
 //var pattern = /[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9) |\w't|\w've|\w'nt|\w's|\w'll|\w're)]/gi; 
 var pattern = /[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9)]/gi;
  //특수문자 모두 공백으로 변환
  readFile = readFile.replace(pattern,',');
  //두개 이상 쉼표는 하나의 쉼표로 변환
-  console.log(readFile);
- readFile = readFile.replace(/\W{2,}/,'');
- console.log(readFile);
+ readFile = readFile.replace(/\,{2,10}/,'');
  // /\s(?=\w)/는 공백 후 문자가 나오는 곳만 참조
  var readFiles = readFile.split(',');
+ serchingText(readFiles);
  return readFiles;
+}
+
+function serchingText(data,findText){
+    var textC = 0;
+    var textInclude = "N";
+    for(var arrayNum in data){
+        if(data[arrayNum] == "was"){textC++;}
+    }
+    if(textC>0){textInclude ="Y";}
+    var out = ["findText : " + findText,"textInclude : " + textInclude,"textCount : " + textC] ;
+    return out;
 }
 
 //소수값 계산
